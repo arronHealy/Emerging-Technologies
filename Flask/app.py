79155ac8eeb8bf6802e5228fc62e8c25948eeef2
arronHealy <G00346883@gmit.ie>
2019-10-22 16:26:23 +0100

    playing around trying to make prediction through flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,16 +1,41 @@
 from flask import Flask, jsonify, request, send_from_directory
-from scipy.misc import imread, imresize, imsave
+from PIL import Image
+
 import tensorflow as tf
 from keras.models import load_model
+
+import keras as kr
 import numpy as np
 import io
 import base64
 
 app = Flask(__name__)
 
-model = load_model('../jupyter/mnist_num_reader.model')
+global model, graph
 
+
+def get_model():
+    m = load_model('../jupyter/mnist_num_reader.model')
+    print('Model loaded')
+    return m
+
+
+print('Loading model & Graph...')
+model = get_model()
 graph = tf.get_default_graph()
+
+
+"""
+def preprocess_image(imageData):
+    canvasImage = Image.open(io.BytesIO(base64.b64decode(imageData)))
+    canvasImage = canvasImage.resize((28, 28), Image.LANCZOS)
+
+    image = Image.new("L", canvasImage.size, (255))
+    image.paste(canvasImage, canvasImage)
+    image = ImageOps.invert(image)
+
+    return image
+"""
 
 
 @app.route('/')
@@ -24,26 +49,25 @@ def classify():
 
     data = request.get_json()
 
-    img = base64.b64decode(data['imageData'])
+    encoded = data["imageData"]
 
-    # x = imresize(img, (28, 28)) / 255
+    img = Image.open(io.BytesIO(base64.b64decode(encoded)))
 
-    # x = x.reshape(1, 28, 28)
+    img = img.resize(img_size, Image.ANTIALIAS)
 
-    # image_file = 'test.png'
+    img = img.convert('1')
 
-    # with open(image_file, 'wb') as f:
-    # f.write(img)
+    img_array = np.asarray(img)
+    img_array = img_array.flatten()
 
-
-"""
     with graph.as_default():
-        result = model.predict(x)
-        data = {
-            'results': result
+        out = model.predict(img_array)
+        print(out)
+        print(np.argmax(out, axis=1))
+        response = {
+            'prediction': np.array_str(np.argmax(out, axis=1))
         }
         return jsonify(response)
-        """
 
 
 if __name__ == '__main__':
